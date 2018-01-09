1ee43860ffb98d90df518a60092fc86553f35ca6
tios <dev.tio.saputra@gmail.com>
2018-01-09 17:46:57 +0700

    First Commit

---
+++ app/src/main/java/com/recycle/view/tios/recycleview/Contact.java
@@ -0,0 +1,37 @@
+package com.recycle.view.tios.recycleview;
+
+import java.util.ArrayList;
+
+/**
+ * Created by tios on 09/01/18.
+ */
+
+public class Contact {
+    private String mName;
+    private boolean mOnline;
+
+    public Contact(String name, boolean online) {
+        mName = name;
+        mOnline = online;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public boolean isOnline() {
+        return mOnline;
+    }
+
+    private static int lastContactId = 0;
+
+    public static ArrayList<Contact> createContactsList(int numContacts) {
+        ArrayList<Contact> contacts = new ArrayList<Contact>();
+
+        for (int i = 1; i <= numContacts; i++) {
+            contacts.add(new Contact("Person " + ++lastContactId, i <= numContacts / 2));
+        }
+
+        return contacts;
+    }
+}
---
+++ app/src/main/java/com/recycle/view/tios/recycleview/MainActivity.java
@@ -0,0 +1,34 @@
+package com.recycle.view.tios.recycleview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList<Contact> contacts;
+    RecyclerView rvContacts;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // ...
+        // Lookup the recyclerview in activity layout
+        rvContacts = (RecyclerView) findViewById(R.id.rvContacts);
+
+
+        // Initialize contacts
+        contacts = Contact.createContactsList(20);
+        // Create adapter passing in the sample user data
+        ContactsAdapter adapter = new ContactsAdapter(this, contacts);
+        // Set layout manager to position the items
+        rvContacts.setLayoutManager(new LinearLayoutManager(this));
+        // Attach the adapter to the recyclerview to populate items
+        rvContacts.setAdapter(adapter);
+        // That's all!
+    }
+}
